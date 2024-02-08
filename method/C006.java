6fc149092f92737cda3d3c5e2b2861597b375253
AmiOshima0130 <142062382+AmiOshima0130@users.noreply.github.com>
2024-02-08 15:38:36 +0900

    Update C006.java
    2/8途中

--- method/C006.java
+++ method/C006.java
@@ -1,49 +1,39 @@
-package java_work0202;
+package c006;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class C006 {
-
+	//メインで実行させるクラス
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();//パラメーター入力
-		int M = sc.nextInt();//ユーザーの人数入力
-		int K = sc.nextInt();//トップK入力
-		
-		Ranking ranking = new Ranking(N);
-		//アイテム種別毎の得点入力・ユーザーのアイテム持つ数入力
-		ranking.inputPoint();
-		ranking.showResult();
-
-	}
-	
-	
-
-}
-
-class Ranking {
-	//フィールド
-	int N;//パラメーター
-	
-	public Ranking(int N) {
-		this.N = N;
-		
-	}
-	
-	
-	//アイテム種別毎の得点入力メソッド
-		public void inputPoint() {
+			
 			Scanner sc = new Scanner(System.in);
-			for(int i = 0; i < N; i++) {
-				int C = sc.nextInt();//アイテム種別毎の得点
-				int X = sc.nextInt();//ユーザーのアイテム持つ数
+			
+			//パラメーターを定義・入力させる
+			int parameter = sc.nextInt();
+			//ユーザーの人数を入力させる
+			int users = sc.nextInt();
+			//トップ○を入力させる
+			int top = sc.nextInt();
+			
+			int points[] = new int[parameter];//pointを配列に入れる
+			
+			//アイテムの種別ごとの得点・ユーザーごとのアイテム持ち数を入力させる
+			for(int i = 0; i < parameter; i++) {
+				int point = sc.nextInt();
+				points[i] = point; // 入力された値をpoints配列に格納
+				
+				//入力したpointsをArrayListに入れる
+				ArrayList<Integer> pointsArray  = new ArrayList<Integer>();
+				for(int j = 0; j < users; j++) {
+					int items = sc.nextInt();
+				}
+				
 			}
-		}
+	}
+
+	//ユーザーのスコアを計算するメソッド
+	public int Calculate(int points, int items) {
 		
-	//結果を表示するメソッド
-		public void showResult() {
-	        // 結果を表示する処理を書く
-	    }
+	}
 }
